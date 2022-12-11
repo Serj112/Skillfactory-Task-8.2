4c3ced54f6cdb08c8145660125ec029b4d2e9dd2
Serj112 <raskolnikov.112@mail.ru>
2022-12-11 19:01:06 +0300

    Add project files.

---
+++ Skillfactory Task 8.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using Skillfactory_Task_8._2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"C:\\Skillfactory\";
+        DirectoryInfo dirinfo = new DirectoryInfo(path);
+        WriteFolderInfo(dirinfo);
+    }
+
+    static void WriteFolderInfo(DirectoryInfo folder)
+    {
+        try
+            {
+                Console.WriteLine(folder.Name + $"- {DirectoryExtension.DirSize(folder)} байт");
+            }
+        
+        catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        
+    }
+
+}
